4f308cf7d9b3bf145f8cd86820a86c4d3d4ee10f
Etienne Bonnassieux <ebonnassieux@synchrotron.localdomain>
2026-02-25 14:31:25 +0100

    cleaned up

--- src/data3gc/sky.py
+++ src/data3gc/sky.py
@@ -17,17 +17,16 @@ import matplotlib.pyplot as plt
 import regions
 
 ### functionalities to add:
-###  - multisky (join Sky objects, show becomes healpix?)
-###  - genuine facet-as-subsky (currently it's all indices, kind of shitty)
+###  - !!! read/write !!! super important
 ###  - generate Tessel layer for backwards compatibility
 ###  - group facets by Voronoi tesselation of Model data slice
-###  - !!! read/write !!! super important
+###  - multisky (join Sky objects, show becomes healpix?)
 
 ### below is experimental for Stokes. over-engineering in practice, but was a personal test.
 type StokesEigen = Literal['I','Q','U','V']
 from typing import Literal
 type Stokes = StokesEigen | tuple[StokesEigen] | tuple[StokesEigen, StokesEigen] | tuple[StokesEigen, StokesEigen, StokesEigen] | tuple[StokesEigen, StokesEigen, StokesEigen, StokesEigen]
-
+### TODO; below, change stokes back to a string of which we look for IQUV subsets...
 
 class Sky:
     '''
@@ -35,12 +34,19 @@ class Sky:
     Sky attributes:
         :param centrecoords: input SkyCoord for this sky's center coordinates 
         :type centrecoords: SkyCoord
-        npix
-        cellsize
-        freqs
-        nfacets
-        stokes
-        skyname
+        :param npix: size of the sky grid, defined as a square 
+        :type npix: int
+        :param cellsize: angular size of individual pixels, in angles (e.g. arcsec)
+        :type cellsize: u.Quantity
+        :param freqs: List of frequencies for each spectral cube slice
+        :type freqs: list[u.Quantity]
+        :param nfacets: Create sub-skies (facets) defined automatically as a square grid. nfacets defines how many facets to create along 1 axis; a total of nfacets^2 facets are created for the sky. 
+        :type nfacets: int
+        :param stokes: Stokes parameters of the sky, which can be one or more values of I,Q,U,V.
+        :type stokes: Stokes="I"
+        :param skyname: Name given to this patch of sky. 
+        :type skyname: str="Sky"
+    If nfacets!=0, facets are an important attribute generated.
         facets
     Facet attributes:
         ...
@@ -78,8 +84,8 @@ class Sky:
         # initialise the data grids
         self.initdata()
 
-        ### DEBUG
-        self.data["restored"] = fits.open("/home/ebonnassieux/OJ287_averaged_outer_uvcut_0.8arcsec-MFS-image.fits")[0].data
+#        ### DEBUG
+#        self.data["restored"] = fits.open("/home/ebonnassieux/OJ287_averaged_outer_uvcut_0.8arcsec-MFS-image.fits")[0].data
 
         # Initialise the coordinate grids in ra, dec and l,m
         coordgrid = np.meshgrid(np.arange(self.npix),np.arange(self.npix))
@@ -104,7 +110,6 @@ class Sky:
                 facet_phasecenter = self.facet_phasecenters[facet_index]
                 print(facet_phasecenter)
                 facetname = facet_phasecenter.to_string('hmsdms')
-                ### todo: why the fuck are the below floats???
                 verts = self.facetvertices[facet_index]
                 xmin,xmax = int(np.min(verts.x)),int(np.max(verts.x))
                 ymin,ymax = int(np.min(verts.y)),int(np.max(verts.y))
@@ -120,9 +125,9 @@ class Sky:
                                         stokes=self.stokes
                 )
                 ### TODO: set up so we do the below only once...
+                # reinitialise regions (update visuals, set gridreg to sky wcs)
                 self.facets[facetname].sky_reg =  self.facet_sky_regs[facet_index]
                 self.facets[facetname].grid_reg =  self.facet_grid_regs[facet_index]
-                # reinitialise regions (update visuals, set gridreg to sky wcs)
                 # define facet grid properties
                 self.facets[facetname].imshape = (len(freqs),len(stokes),xlen, ylen)
                 self.facets[facetname].wcs     = WCS(self.facets[facetname].wcs_input_dict())
@@ -132,7 +137,6 @@ class Sky:
                 verts = self.facetvertices[facet_index]
                 xmin,xmax = int(np.min(verts.x)),int(np.max(verts.x))
                 ymin,ymax = int(np.min(verts.y)),int(np.max(verts.y))
-                
                 sky_to_facet_regmask = np.zeros(self.imshape).astype(bool)
                 sky_to_facet_regmask[:,:,ymin:ymax,xmin:xmax]=True
                 # fill data
@@ -155,12 +159,6 @@ class Sky:
         ### TODO: define specific shared-xarray image format?
         ### TODO: figure out if we want these to be HDUS? Probably not actually...
         self.data={}
-        # self.data["dirty"]    = self.returnHDU("dirty")
-        # self.data["restored"] = self.returnHDU("restored")
-        # self.data["residual"] = self.returnHDU("residual")
-        # self.data["model"]    = self.returnHDU("model")
-        # self.data["mask"]     = self.returnHDU("mask")
-        # self.data["beam"]     = self.returnHDU("beam")
         keys = {"dirty",
                 "restored",
                 "residual",
@@ -200,42 +198,46 @@ class Sky:
     def show(self,
              datakey: str="restored",
              plot_facets: list | str="all",
+             channel=0,
+             stokes=0,
              vmin: float=-8.e-5,
              vmax: float=2.e-4
              ):
         # this should imshow the sky and its divisions.
-        ### TODO: show facets by name / group / sky area rather than index!
         # set up the plot axes etc
         fig, ax = plt.subplots(subplot_kw=dict(projection=self.gridwcs))
         ax.set(xlabel='Right Ascension', ylabel='Declination',title=self.name)
         # get data as numpy array
         data = self.data[datakey]
-        ax.imshow(data[0,0,:,:], vmin=vmin, vmax=vmax, origin='lower',alpha=0.1)
+        # plot the overall sky box region
+        self.grid_reg.plot(ax=ax)
         if self.nfacets==0:
-            ax.imshow(data[0,0,:,:], vmin=vmin, vmax=vmax, origin='lower')           
+            # in this scenario, simply show the data and region
+            ax.imshow(data[channel,stokes,:,:], vmin=vmin, vmax=vmax, origin='lower')           
         else:
             if plot_facets=="all":
                 # only plot the facet grid
-                ax.imshow(data[0,0,:,:], vmin=vmin, vmax=vmax, origin='lower')
-                #ax.grid(color='white', ls='solid')
-                self.grid_reg.plot(ax=ax)
-                #for facet_reg in self.facet_grid_regs:f
-                #    facet_reg.plot(ax=ax)
+                ax.imshow(data[channel,stokes,:,:], vmin=vmin, vmax=vmax, origin='lower')
+                # plot the facet regions only. No facet-based overplot, since we want everything...
                 for facet_key in self.facets.keys():
                     self.facets[facet_key].grid_reg.plot(ax=ax)
             else:
+                # get the keys associated to the integers requested
                 plotkeys=[]
                 keylist = list(self.facets.keys())
                 for plot_facet in plot_facets:
                     plotkeys.append(keylist[plot_facet])
+                # plot the individual facet grid + region
                 for facet_key in plotkeys:
                     this_facet = self.facets[facet_key]                    
-                    ax.imshow(this_facet.data[datakey][0,0,:,:],
+                    ax.imshow(this_facet.data[datakey][channel,stokes,:,:],
                                     transform=ax.get_transform(this_facet.gridwcs),
-                                    vmin=vmin, vmax=vmax, origin="lower")
+                                    vmin=vmin, 
+                                    vmax=vmax, 
+                                    origin="lower")
                     this_facet.grid_reg.plot(ax=ax)
-        ax.imshow(data[0,0,:,:], vmin=vmin, vmax=vmax, origin='lower',alpha=0.6)
-        self.grid_reg.plot(ax=ax)
+                # underlay the full sky
+                ax.imshow(data[channel,stokes,:,:], vmin=vmin, vmax=vmax, origin='lower',alpha=0.6)
         plt.show()
     
     ### facet initialisation functionalities
@@ -267,8 +269,6 @@ class Sky:
                                                    height = self.bin_sizes[i],
                                                    visual=reg_visuals
                                                    )
-            # grid_reg = regions.PolygonPixelRegion(vertices=self.facetvertices[i],
-            #                                       visual=reg_visuals)
             sky_reg = grid_reg.to_sky(self.gridwcs)
             facet_sky_regs.append(sky_reg)
             facet_grid_regs.append(grid_reg)
@@ -294,7 +294,7 @@ class Sky:
                  wcs:WCS
                  ):
         '''
-        Docstring for ImageHDU
+        Generates HDU object from data and WCS for writing purposes
         
         :param self: class Sky
         :param name: Name of the HDU, restored, residual, model etc
@@ -304,7 +304,7 @@ class Sky:
         :param wcs: WCS of the HDU
         :type wcs: WCS
         '''
-        this_hdu      = fits.PrimaryHDU(data:=data, 
+        this_hdu      = fits.PrimaryHDU(data=data, 
                                         header=wcs.to_header())
         this_hdu.name = name
         return this_hdu
@@ -398,25 +398,17 @@ class Sky:
         }
         return wcs_input_dict
 
-    # def debugWCS(self):
-    #     '''
-    #     Docstring for debugWCS
-    #     This is a function to test our WCS, for debug purposes
-    #     :param self: Description
-    #     '''
-    #     self.centrecoords=SkyCoord(133.703625*u.deg,20.1085*u.deg,frame="fk5")
-    #     self.npix=1000
-    #     self.cellsize=0.1*u.arcsec
-    #     self.imshape = (len(self.freqs),len(self.stokes),self.npix, self.npix)
-    #     self.wcs  = WCS(self.wcs_input_dict())
-    #     self.data = fits.open("/home/ebonnassieux/OJ287_averaged_outer_uvcut_0.8arcsec-MFS-image.fits")[0].data
-    #     self.hdu = fits.PrimaryHDU(data=self.data, header=self.wcs.to_header())
-    #     self.hdu.writeto('example.fits',overwrite=True)
-    #     print("The two images should be exactly the same")
-    #     print("dsm /home/ebonnassieux/OJ287_averaged_outer_uvcut_0.8arcsec-MFS-image.fits Data3GC/example.fits")
+    def write(self,filename):
+        '''docstring'''
+        print("TODO")
+
+    def read(filename):
+        '''docstring'''
+        print("TODO")
 
 
 def generate_vertices(edges):
+    '''docstring'''
     vertices=[]
     for ifacet in range(len(edges)-1):
         for jfacet in range(len(edges)-1):
@@ -434,6 +426,7 @@ def generate_vertices(edges):
     return vertices
 
 def MaskFromVertices(data,vertices):
+    '''docstring'''
     if len(data)==2:
         d = np.zeros_like(data).astype(bool)
     else:
