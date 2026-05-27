c88959608bd949d9ae12858d9a5a0ed6210bca76
ebonnassieux <etienne.bonnassieux@u-bordeaux.fr>
2026-05-27 10:11:13 +0200

    can initialise from fits file

--- tests/test_sky_ndarray.py
+++ tests/test_sky_ndarray.py
@@ -6,15 +6,38 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import pytest
 import pathlib
+# to plot random facets
+import random
 
 
 from data3gc.sky_ndarray import Sky
 
 # initialise the sky object from test fits file
-p = pathlib.Path("/home/bonnassieux/Downloads/M31-lowres-LOFAR.fits")
+p = pathlib.Path("tests/Data/M31-lowres-LOFAR-cropped.fits")
 filename=p.absolute().as_posix()
-test_sky = Sky.from_fits(filename,
-                         nfacets=2)
+test_sky_m31_cropped = Sky.from_fits(filename,
+                                     nfacets=5)
+test_sky_m31_cropped.show(vmin=-0.0005,vmax=0.0015)
+edit_facets = [0,7,]
+keylist = list(test_sky_m31_cropped.facets.keys())#[edit_facets]
+for i, key in enumerate(keylist):
+    if i in edit_facets:
+        this_facet_data = test_sky_m31_cropped.facets[key].data["restored"]
+        # add noise to this facet to show we can edit specific areas
+        noisevals = float(i+1)*np.random.normal(loc=0,
+                                            scale=0.01,
+                                            size=this_facet_data.shape)
+        test_sky_m31_cropped.facets[key].data["restored"] = this_facet_data + noisevals
+test_sky_m31_cropped.show(plot_facets=list(edit_facets),vmin=-0.0005,vmax=0.0015)
+# update sky with facet information
+test_sky_m31_cropped.update()
+test_sky_m31_cropped.show(vmin=-0.0005,vmax=0.0015)
+
+
+# exit gracefully
+test_sky_m31_cropped.close()
+
+
 
 
 
@@ -32,26 +55,25 @@ test_sky = Sky.from_fits(filename,
 #test_sky.show()
 
 # plot random facets
-import random
 #show_facets = random.sample(range(0,test_sky.nfacets**2),15)
 #test_sky.show(plot_facets=list(show_facets))
 
-# edit a facet and overplot it
-edit_facets = [0,1,]
-keylist = list(test_sky.facets.keys())#[edit_facets]
-for i, key in enumerate(keylist):
-    if i in edit_facets:
-        this_facet_data = test_sky.facets[key].data["restored"]
-        # add noise to this facet to show we can edit specific areas
-        noisevals = float(i+1)*np.random.normal(loc=0,
-                                            scale=0.01,
-                                            size=this_facet_data.shape)
-        test_sky.facets[key].data["restored"] = this_facet_data + noisevals
-#test_sky.show(plot_facets=list(edit_facets))
-# update sky with facet information
-test_sky.update()
-test_sky.show()
+# # edit a facet and overplot it
+# edit_facets = [0,1,]
+# keylist = list(test_sky.facets.keys())#[edit_facets]
+# for i, key in enumerate(keylist):
+#     if i in edit_facets:
+#         this_facet_data = test_sky.facets[key].data["restored"]
+#         # add noise to this facet to show we can edit specific areas
+#         noisevals = float(i+1)*np.random.normal(loc=0,
+#                                             scale=0.01,
+#                                             size=this_facet_data.shape)
+#         test_sky.facets[key].data["restored"] = this_facet_data + noisevals
+# #test_sky.show(plot_facets=list(edit_facets))
+# # update sky with facet information
+# test_sky.update()
+# test_sky.show(vmin=-0.0005,vmax=0.0015)
 
 
-# exit gracefully
-test_sky.close()
+# # exit gracefully
+# test_sky.close()
